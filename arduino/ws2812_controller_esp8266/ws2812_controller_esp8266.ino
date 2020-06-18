8c3e870e9595d5309b2cecef59ecb5824fd68835
Spencer McDonough <shmcdono@usc.edu>
2020-06-17 22:11:56 -0700

    config and esp set to top shelf

--- arduino/ws2812_controller_esp8266/ws2812_controller_esp8266.ino
+++ arduino/ws2812_controller_esp8266/ws2812_controller_esp8266.ino
@@ -4,7 +4,7 @@
 #include <NeoPixelBus.h>
 
 // Set to the number of LEDs in your LED strip
-#define NUM_LEDS 60
+#define NUM_LEDS 86
 // Maximum number of packets to hold in the buffer. Don't change this.
 #define BUFFER_LEN 1024
 // Toggles FPS output (1 = print FPS over serial, 0 = disable output)
@@ -14,8 +14,8 @@
 const uint8_t PixelPin = 3;  // make sure to set this to the correct pin, ignored for Esp8266(set to 3 by default for DMA)
 
 // Wifi and socket settings
-const char* ssid     = "YOUR_WIFI_SSID";
-const char* password = "YOUR_WIFI_PASSWORD";
+const char* ssid     = "Top Shelf";
+const char* password = "purpskurp69";
 unsigned int localPort = 7777;
 char packetBuffer[BUFFER_LEN];
 
@@ -26,9 +26,9 @@ WiFiUDP port;
 
 // Network information
 // IP must match the IP in config.py in python folder
-IPAddress ip(192, 168, 0, 150);
+IPAddress ip(192, 168, 1, 150);
 // Set gateway to your router's gateway
-IPAddress gateway(192, 168, 0, 1);
+IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 void setup() {
